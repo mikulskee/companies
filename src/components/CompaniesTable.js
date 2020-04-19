d4180f93fec953bb8652a432baf1d46e97e96ae9
Mateusz Mikulski <mikulskee@gmail.com>
2020-04-19 11:09:44 +0200

    implemented parsing API data & display it to table

--- src/components/CompaniesTable.js
+++ src/components/CompaniesTable.js
@@ -2,23 +2,28 @@ import React, { useContext } from "react";
 import { CompaniesContext } from "../contexts/CompaniesContext";
 
 const CompaniesTable = () => {
-  const { companies } = useContext(CompaniesContext);
+  const { fullData } = useContext(CompaniesContext);
 
-  return (
-    <>
-      {companies ? (
-        companies.map((company) => (
-          <li key={company.id}>
-            <h3>
-              {company.id}. Name:{company.name} City:{company.city}
-            </h3>
-          </li>
-        ))
-      ) : (
-        <h1>Ładowanie...</h1>
-      )}
-    </>
-  );
+  const listOfCompanies = fullData.map((company) => (
+    <li key={company.id}>
+      <h3>
+        {company.id}. <br />
+        Name: {company.name}
+        <br /> City: {company.city}
+        <br />
+        Total Income: {company.totalIncome}
+        <br />
+        Average Income: {company.average} <br />
+        Last Month Income:{" "}
+        {company.lastMonth
+          .map((item) => item.value)
+          .reduce((a, b) => parseFloat(a) + parseFloat(b), 0)
+          .toFixed(2)}
+      </h3>
+    </li>
+  ));
+
+  return <>{fullData.length > 0 ? listOfCompanies : <h1>Ładowanie...</h1>}</>;
 };
 
 export default CompaniesTable;
--- src/contexts/CompaniesContext.js
+++ src/contexts/CompaniesContext.js
@@ -4,10 +4,12 @@ export const CompaniesContext = createContext();
 
 const CompaniesContextProvider = (props) => {
   const [companies, setCompanies] = useState([]);
+  const [stats, setStats] = useState([]);
   const [incomes, setIncomes] = useState([]);
+  const [fullData, setFullData] = useState([]);
 
   const URL = "https://recruitment.hal.skygate.io/companies";
-  const incomesURL = "https://recruitment.hal.skygate.io/incomes/";
+  const statsURL = "https://recruitment.hal.skygate.io/incomes/";
 
   const getCompanies = async () => {
     try {
@@ -18,9 +20,9 @@ const CompaniesContextProvider = (props) => {
       console.log(err);
     }
   };
-  const getIncomes = async (item) => {
+  const getStats = async (item) => {
     try {
-      const response = await fetch(`${incomesURL}${item.id}`);
+      const response = await fetch(`${statsURL}${item.id}`);
       const data = response.json();
       return data;
     } catch (err) {
@@ -31,20 +33,72 @@ const CompaniesContextProvider = (props) => {
   useEffect(() => {
     getCompanies().then((data) => {
       setCompanies(data);
-      const table = data.map((data) => getIncomes(data));
-      Promise.all(table).then((files) => setIncomes(files));
-      setIncomes(table);
+      const table = data.map((data) => getStats(data));
+      Promise.all(table).then((files) => setStats(files));
     });
   }, []);
+
+  useEffect(() => {
+    if (stats.length) {
+      const table = stats
+        .map((item) => ({
+          id: item.id,
+          totalIncome: item.incomes
+            .map((item) => parseFloat(item.value))
+            .reduce((a, b) => a + b, 0)
+            .toFixed(2),
+          average: (
+            item.incomes
+              .map((item) => parseFloat(item.value))
+              .reduce((a, b) => a + b, 0) / item.incomes.length
+          ).toFixed(2),
+          lastMonth: item.incomes
+            .map((item) => ({
+              value: item.value,
+              date: Date.parse(item.date),
+            }))
+            .sort((a, b) => b.date - a.date)
+            .map((item) => ({
+              value: item.value,
+              date: `${new Date(item.date).getMonth()} ${new Date(
+                item.date
+              ).getFullYear()}`,
+            })),
+        }))
+        .map((item) => {
+          let shift = "";
+          shift = item.lastMonth[0].date;
+
+          const table = item.lastMonth.filter((item) => {
+            return item.date === shift;
+          });
+
+          return { ...item, lastMonth: table };
+        });
+
+      setIncomes(table);
+    }
+  }, [stats]);
+
+  useEffect(() => {
+    if (incomes.length > 0) {
+      const dataTable = () => {
+        let hash = new Map();
+        companies.concat(incomes).forEach((obj) => {
+          hash.set(obj.id, Object.assign(hash.get(obj.id) || {}, obj));
+        });
+        const table = Array.from(hash.values());
+
+        setFullData(table);
+      };
+      dataTable();
+    }
+  }, [incomes, companies]);
 
   return (
     <CompaniesContext.Provider
       value={{
-        companies,
-        setCompanies,
-        getCompanies,
-        incomes,
-        setIncomes,
+        fullData,
       }}
     >
       {props.children}
