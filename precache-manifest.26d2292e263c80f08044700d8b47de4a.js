8ff8f16f310e7bf7f0c90044ce37659039231a33
Mateusz Mikulski <mikulskee@gmail.com>
2020-04-21 02:19:51 +0200

    Updates

---
+++ precache-manifest.26d2292e263c80f08044700d8b47de4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "391ae1b1bfcfc572fc79b173a1255524",
+    "url": "/companies/index.html"
+  },
+  {
+    "revision": "110561870386fe742cfb",
+    "url": "/companies/static/css/main.0dd8c42c.chunk.css"
+  },
+  {
+    "revision": "f86ad6bfcabe242d8d73",
+    "url": "/companies/static/js/2.ed08e9d3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/companies/static/js/2.ed08e9d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "110561870386fe742cfb",
+    "url": "/companies/static/js/main.6e191d57.chunk.js"
+  },
+  {
+    "revision": "8c5f20b94edf6af320f2",
+    "url": "/companies/static/js/runtime-main.13331fa0.js"
+  }
+]);
